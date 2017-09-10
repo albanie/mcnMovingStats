0c7724d6eeed6f0e41723dd9d0f7985f4947445d
albanie <albanie@robots.ox.ac.uk>
2017-09-10 14:37:15 +0100

    mods

--- setup_mcnMovingStats.m
+++ setup_mcnMovingStats.m
@@ -1,8 +1,8 @@
-function setup_mcnFeatCovs()
-%SETUP_MCNFEATCOVS Sets up mcnFeatCovs, by adding its folders 
+function setup_mcnMovingStats()
+%SETUP_MCNMOVINGSTATS Sets up mcnMovingStats, by adding its folders 
 % to the Matlab path
 %
-% Copyright (C) 2017 Samuel Albanie
+% Copyright (C) 2017 Samuel Albanie and David Novotny
 % Licensed under The MIT License [see LICENSE.md for details]
 
   root = fileparts(mfilename('fullpath')) ;
