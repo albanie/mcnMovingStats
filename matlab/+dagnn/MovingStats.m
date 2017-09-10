b1395075079a4a0fab01378e0c0dfa34d5f6a785
albanie <albanie@robots.ox.ac.uk>
2017-09-10 13:55:40 +0100

    updates

--- matlab/+dagnn/MovingStats.m
+++ matlab/+dagnn/MovingStats.m
@@ -4,55 +4,46 @@ classdef MovingStats < dagnn.ElementWise
     N = 0
     K = []
     mu = 0
-    mu1 = 0
-    M12 = 0
+    sm1 = 0 % shifted mean
+    sm2 = 0
     cov = 0
     average = 0 
     updateDiff = 0 
     numAveraged = 0
     descCheck = []
+    vis = 0 
   end
 
   methods
     function outputs = forward(obj, inputs, params)
 
       prevCov = obj.cov ;
-      x_ = permute(inputs{1},[3 1 2 4]) ;
-      x_ = reshape(x_, size(x_, 1), []) ;
-      x_ = vl_colsubset(x_, 1e4) ; % restrict sample size for estimate if needed
-      if isempty(obj.K), obj.K = mean(x_(:)) ; end % initialise mean estimate
-      obj.N = obj.N + size(x_, 2) ; % total number of samples seen so far
-      m12_ = (x_- obj.K) * (x_ - obj.K)' ; % covariance estimate from current batch
-      obj.M12 = obj.M12 + m12_ ; % update covariance estimate
-      obj.mu1 = obj.mu1 + sum(x_ - obj.K, 2) ; % update distance to 
-      
-      % if mean(obj.M12(:)) > 1e10
-      %   fprintf('!! inaccurate ... !!\n') ;
-      % end
 
-      obj.cov = (obj.M12 - obj.mu1*obj.mu1'*(1/obj.N))*(1/(obj.N-1)) ;
-      obj.mu = obj.mu1*(1 / obj.N) + obj.K ;
+      % provide an offset to ensure numerical stability (initisalise to mean)
+      if isempty(obj.K), obj.K = mean(inputs{1}(:)) ; end 
+      [obj.sm1, obj.sm2, obj.mu, obj.cov, obj.N] ...
+            = vl_nnmovingstats(inputs{1}, obj.sm1, obj.sM2, obj.N, 'K', obj.K) ;
 
-      % if true
-      %   obj.descCheck  = [obj.descCheck vl_colsubset(x_,100)] ;
-      %   disp(size(obj.descCheck)) ;
-      %   if size(obj.descCheck,2) > 1e4
-      %     xc_ = bsxfun(@minus,obj.descCheck,mean(obj.descCheck,2)) ;
-      %     cv__ = xc_*xc_'/size(xc_,2) ;
-      %     figure(1) ; clf ; 
-      %     subplot(2,3,1) ; imagesc(gather(obj.cov)) ; colorbar ; 
-      %     subplot(2,3,2) ; imagesc(gather(cv__)) ; colorbar ; 
-      %     subplot(2,3,3) ; imagesc(gather(abs(cv__-obj.cov))) ; colorbar ;
-      %     subplot(2,3,4) ; imagesc(gather(obj.mu)) ; colorbar ; 
-      %     subplot(2,3,5) ; imagesc(gather(mean(obj.descCheck,2))) ; colorbar ; 
-      %     subplot(2,3,6) ; imagesc(abs(gather(obj.mu-mean(obj.descCheck,2)))) ; colorbar ;
-      %     saveas(1,'~/junk/covtest.png')
-      %     keyboard
-      %   end
-      % end
+       if obj.vis
+         obj.descCheck  = [obj.descCheck vl_colsubset(x_,100)] ;
+         disp(size(obj.descCheck)) ;
+         if size(obj.descCheck,2) > 1e4
+           xc_ = bsxfun(@minus,obj.descCheck,mean(obj.descCheck,2)) ;
+           cv__ = xc_*xc_'/size(xc_,2) ;
+           figure(1) ; clf ; 
+           subplot(2,3,1) ; imagesc(gather(obj.cov)) ; colorbar ; 
+           subplot(2,3,2) ; imagesc(gather(cv__)) ; colorbar ; 
+           subplot(2,3,3) ; imagesc(gather(abs(cv__-obj.cov))) ; colorbar ;
+           subplot(2,3,4) ; imagesc(gather(obj.mu)) ; colorbar ; 
+           subplot(2,3,5) ; imagesc(gather(mean(obj.descCheck,2))) ; colorbar ; 
+           subplot(2,3,6) ; imagesc(abs(gather(obj.mu-mean(obj.descCheck,2)))) ; colorbar ;
+           saveas(1,'~/junk/covtest.png')
+           keyboard
+         end
+       end
 
-      % Use Frobenius to track changes to Covariance matrixj
-      df = norm(obj.cov(:)- prevCov(:), 'fro') ;
+      % Use Frobenius to track changes to Covariance matrix
+      df = norm(obj.cov(:) - prevCov(:), 'fro') ;
       obj.updateDiff = df ;
 
       n_ = obj.numAveraged  ;
@@ -63,8 +54,8 @@ classdef MovingStats < dagnn.ElementWise
     end
 
     function reset(obj)
-      obj.mu1 = 0  ;
-      obj.M12 = 0  ;
+      obj.sm1 = 0  ;
+      obj.sm2 = 0  ;
       obj.mu = 0  ;
       obj.cov = 0  ;
       obj.K = [] ;
--- matlab/vl_nnmovingstats.m
+++ matlab/vl_nnmovingstats.m
@@ -1,26 +1,57 @@
-function [mu, cov] = vl_nnmovingstats(x, pMu, pCov, N, varargin)
-%VL_NNMOVINGSTATS rolling mean and covariance estimates
-% [MU,COV] = VL_NNMOVINGSTATS(X, PMU, PCOV) 
+function [sm1, sm2, mu, cov, N] = vl_nnmovingstats(x, psm1, psm2, N, varargin)
+%VL_NNMOVINGSTATS rolling statistical estimates
+%   [MU,COV] = VL_NNMOVINGSTATS(X, PSM1, PSM2, N, VARARGIN) takes in 
+%   a tensor X, previous estimates of the shifted first moment PSM1 and 
+%   shifted second moment PSM2 and computes updated first and second order
+%   shifted statistics SM1 and SM2, together with estimates MU and COV of the
+%   mean and covariance of X. X is assumed to be an H x W x C x N tensor,
+%   in which we consider each channel to be the feature of interest (so that
+%   different spatial locations/batches correspond to different samples). N
+%   is an integer tracking the total number of samples used in the estimate
+%   so far.
+%
+%   The function tracks shifted first and second moments (rather than directly
+%   tracking the mean and covariance of the features) to avoid issues with 
+%   numerical instability, which can occur when the magnitude of the covariance
+%   is small relative to the mean.
+%
+%   VL_NNMOVINGSTATS(..., 'option', value, ...) accepts the following
+%   options:
+%
+%   `K`:: []
+%    The "shift" factor used to ensure numerical stability.  If unset, it will
+%    be set to the mean of the current sample, X.
+%
+%   `BESSEL`:: true
+%    If true, will apply Bessel correction to the estimate of covariance (to 
+%    account for differences in sample/popluation statistics).
+%
+%   `MAXSAMPLE`:: 1e4
+%    Sets a maximum number of samples to be included in the estimation of the
+%    statistic update (these samples will be selected uniformly from X).
+%
+% Copyright (C) 2017 Samuel Albanie and David Novotny
+% Licensed under The MIT License [see LICENSE.md for details]
 
   opts.K = [] ;
-  opts.maxSample = 1e4 ;
   opts.bessel = true ;
+  opts.maxSample = 1e4 ;
   [opts, dzdy] = vl_argparsepos(varargin) ;
 
   % Use the sample mean for K if not provided
-  K = mean(x_(:)) ;
+  if isempty(opts.K), K = mean(x(:)) ; else, K = opts.K ; end
 
   numChannels = size(x, 3) ;
   x = permute(x, [3 1 2 4]) ;
-  x = reshape(x, numChannels, []) ;
-  N = N + size(x, 2) ; % update number of samples seen
+  x = reshape(x, numChannels, []) ; % form data matrix
+  N = N + size(x, 2) ; % keep track number of samples seen
 
-  batchCov = (x - K) * (x - K)' ;
-  M12 = M12 + batchCov ;
-  residuals = residuals + sum(x - K, 2) ;
+  sm2_delta = (x - K) * (x - K)' ; % compute update to shifted second moment
+  sm2 = psm2 + sm2_delta ;
+  sm1 = psm1 + sum(x - K, 2) ;
 
   if opts.bessel, denom = N - 1 ; else, denom = N ; end
-  cov = (M12 - (residuals * residuals' / N)) / denom ; % use Bessel correction
-  mu = K + (residuals / N) ;
+  cov = (sm2 - (sm1 * sm1' / N)) / denom ; % use Bessel correction
+  mu = K + (sm1 / N) ;
 
   assert(isempty(dzdy), 'moving stats only supports forward computation') ;
